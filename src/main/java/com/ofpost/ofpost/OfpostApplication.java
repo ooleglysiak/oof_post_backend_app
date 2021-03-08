6e7950ed20d6d8c5e48f00b21404026720af6597
OlegLysiak <lysiak.ob@gmail.com>
2021-03-08 06:45:24 +0200

    marge initial commit

---
+++ src/main/java/com/ofpost/ofpost/OfpostApplication.java
@@ -0,0 +1,13 @@
+package com.ofpost.ofpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ofpost/ofpost/OfpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofpost.ofpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
